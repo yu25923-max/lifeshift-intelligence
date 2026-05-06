b8f3e18bb74cb54f93c2ccb727933cc3e68a17db
yu25923-max <yu25923@gmail.com>
2026-05-06 11:26:12 +0900

    Update gemini.js

--- api/gemini.js
+++ api/gemini.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
     }));
 
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-latest:generateContent
+`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-latest:generateContent
 ?key=${apiKey}`,
       {
         method: 'POST',
